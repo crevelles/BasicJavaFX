4a42426be4e6a454b3531fe9dc5ce69cadac7842
Cristobal Revelles <revellestrabajo@hotmail.com>
2017-10-27 12:40:17 +0200

    22

--- BasicJavaFX/src/view/Controler.java
+++ BasicJavaFX/src/view/Controler.java
@@ -3,11 +3,38 @@ package view;
 import javafx.fxml.FXML;
 import javafx.scene.effect.GaussianBlur;
 import javafx.scene.image.ImageView;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Ellipse;
 
 public class Controler {
 	
 	@FXML
-    private ImageView imageView; 
+    private ImageView imageView;   
+	private Ellipse ellipse;
+	private Circle circle;
+
+	
+	@FXML
+    public void setclip() {           
+        ellipse = new Ellipse();
+//        circle = new Circle((imageView.getFitWidth())*0.2);
+
+        ellipse.centerXProperty().setValue(95);
+        ellipse.centerYProperty().setValue(70);
+        ellipse.radiusXProperty().setValue(70);
+        ellipse.radiusYProperty().setValue(70);
+        
+//        importing and modifying sizes
+//    	ellipse.radiusXProperty().setValue(imageView.getFitWidth()*0.1);
+//    	ellipse.radiusYProperty().setValue(imageView.getFitHeight()*0.17);
+        
+        imageView.setClip(ellipse);
+
+////apply a shadow effect.
+//        imageView.setEffect(new DropShadow(120, Color.BLACK));
+
+    }
+
 	@FXML
     public void setblur() {           
              imageView.setEffect(new GaussianBlur(10));
